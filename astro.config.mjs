94285f40e21df41678994879ab1411225d95d4f0
Jonathon Toon <toon@Jonathons-MacBook-Pro.local>
2024-05-28 17:41:07 +1200

    init

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import compress from "astro-compress";
+
+// https://astro.build/config
+export default defineConfig({
+  trailingSlash: "always",
+  integrations: [icon({
+    iconDir: "src/assets/svgs"
+  }), tailwind(), compress()]
+});
