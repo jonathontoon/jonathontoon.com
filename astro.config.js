1d895c36e87b593b5a9168ff72810d695d89d960
Jonathon Toon <toon@Jonathons-MBP.lan>
2024-06-13 09:30:36 +1200

    cloudflare

--- astro.config.js
+++ astro.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 import icon from "astro-icon";
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: cloudflare(),
   trailingSlash: "always",
   integrations: [
     icon({
