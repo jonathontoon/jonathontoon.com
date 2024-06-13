ddfd74fa1c044efd28ddfd51ab55c90298dfab2b
Jonathon Toon <toon@Jonathons-MBP.lan>
2024-06-13 12:39:48 +1200

    updates

--- astro.config.js
+++ astro.config.js
@@ -4,16 +4,14 @@ import cloudflare from "@astrojs/cloudflare";
 import icon from "astro-icon";
 import compress from "astro-compress";
 
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   trailingSlash: "always",
-  integrations: [
-    icon({
-      iconDir: "src/assets/svgs"
-    }),
-    tailwind(),
-    compress()
-  ],
+  integrations: [icon({
+    iconDir: "src/assets/svgs"
+  }), tailwind(), compress(), purgecss()],
   adapter: cloudflare()
 });
