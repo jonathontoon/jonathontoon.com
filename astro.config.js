c1b3eee739e7d2e499069bf2f9bb7aece46eb464
Jonathon Toon <toon@Mac.lan>
2025-08-30 16:32:37 +1200

    update image

--- astro.config.js
+++ astro.config.js
@@ -3,15 +3,42 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import compress from "astro-compress";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.jonathontoon.com",
+  output: "static",
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
   integrations: [
     icon({
       iconDir: "public/images/icons"
     }),
     tailwind(),
     mdx(),
-    compress()
-  ]
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date()
+    }),
+    robotsTxt({
+      sitemap: true
+    }),
+    compress({
+      CSS: true,
+      HTML: true,
+      Image: true,
+      JavaScript: true,
+      SVG: true
+    })
+  ],
+  image: {
+    domains: ["jonathontoon.com", "www.jonathontoon.com"]
+  }
 });
