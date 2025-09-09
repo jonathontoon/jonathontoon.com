8aaf8d170427b26c80e80b59cb0edf81af911b3e
Jonathon Toon <toon@Mac.lan>
2025-09-10 09:50:36 +1200

    remove

--- astro.config.js
+++ astro.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import umami from "@yeskunall/astro-umami"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.jonathontoon.com",
   output: "static",
   integrations: [
-    umami({ id: "e1d957f7-6fdb-4665-885c-fd9ec7fa871a" }),
-    icon({
+   icon({
       iconDir: "public/images/icons"
     }),
     tailwind(),
