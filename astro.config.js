d6d52102ea499ad2912fdbe8ef3122953d3a1333
Jonathon Toon <toon@Mac.lan>
2025-09-10 09:04:47 +1200

    simplify

--- astro.config.js
+++ astro.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import compress from "astro-compress";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import robotsTxt from "astro-robots-txt";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
@@ -21,22 +17,6 @@ export default defineConfig({
       iconDir: "public/images/icons"
     }),
     tailwind(),
-    mdx(),
-    sitemap({
-      changefreq: "weekly",
-      priority: 0.7,
-      lastmod: new Date()
-    }),
-    robotsTxt({
-      sitemap: true
-    }),
-    compress({
-      CSS: true,
-      HTML: false,
-      Image: true,
-      JavaScript: false,
-      SVG: true
-    })
   ],
   image: {
     domains: ["jonathontoon.com", "www.jonathontoon.com"]
