deaa49bf43d071249ae96e31ea1315f0716ed9f7
Jonathon Toon <toon@Mac.lan>
2025-09-10 09:18:08 +1200

    okay

--- astro.config.js
+++ astro.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.jonathontoon.com",
-  output: "server",
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  }),
+  output: "static",
   integrations: [
     icon({
       iconDir: "public/images/icons"
