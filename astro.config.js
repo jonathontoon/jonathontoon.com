04d9551399ba481d250b0ca0f4e4403d16413927
Jonathon Toon <toon@Mac.lan>
2025-09-10 09:43:56 +1200

    update

--- astro.config.js
+++ astro.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import umami from "@yeskunall/astro-umami"
 
 // https://astro.build/config
 export default defineConfig({
