42d5ea8f734ddad59d28f6e9a5aca1f405ea46e0
Jonathon Toon <toon@Mac.lan>
2025-09-24 14:38:31 +1200

    update images

--- astro.config.js
+++ astro.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.jonathontoon.com",
   output: "static",
+  adapter: cloudflare({
+    imageService: 'compile'
+  }),
   integrations: [
    icon({
       iconDir: "public/images/icons"
@@ -13,6 +17,19 @@ export default defineConfig({
     tailwind(),
   ],
   image: {
-    domains: ["jonathontoon.com", "www.jonathontoon.com"]
+    service: {
+      entrypoint: "astro/assets/services/sharp",
+      config: {
+        limitInputPixels: false,
+      },
+    },
+    domains: ["jonathontoon.com", "www.jonathontoon.com"],
+    formats: ["webp", "avif"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.jonathontoon.com"
+      }
+    ]
   }
 });
