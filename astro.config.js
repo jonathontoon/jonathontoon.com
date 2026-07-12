2074686a30a03fcd27d2c371fbdcfe6ac39e4800
Jonathon Toon <hi@jonathontoon.com>
2026-07-12 14:01:51 +1200

    update

--- astro.config.js
+++ astro.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.jonathontoon.com",
   output: "static",
-  adapter: cloudflare({
-    imageService: 'compile'
-  }),
   integrations: [
    icon({
       iconDir: "public/images/icons"
